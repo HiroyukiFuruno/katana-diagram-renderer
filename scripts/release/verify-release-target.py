e771bfd7c30d722762edb2ac0df03802e932fd94
HiroyukiFuruno <hfuruno0114@gmail.com>
2026-08-28 09:52:49 +0900

    release: v0.4.18を準備する

--- scripts/release/verify-release-target.py
+++ scripts/release/verify-release-target.py
@@ -12,8 +12,8 @@ import sys
 from dataclasses import dataclass
 from urllib import error, request
 
-REQUIRED_LATEST_RELEASE = "v0.4.16"
-REQUIRED_TARGET_RELEASE = "v0.4.17"
+REQUIRED_LATEST_RELEASE = "v0.4.17"
+REQUIRED_TARGET_RELEASE = "v0.4.18"
 
 
 @dataclass(frozen=True, order=True)
